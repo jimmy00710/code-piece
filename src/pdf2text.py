4cec2bcb8651f490cd1dc3ecc8f023428d67635b
bitroid <jim00710@yahoo.com>
2020-12-19 15:00:02 +0530

    Pyfiles for converting pdf to text and a code file to search all files in a given directory

---
+++ src/pdf2text.py
@@ -0,0 +1,16 @@
+import os 
+from pdfminer.high_level import extract_text
+
+'''
+Input - filepath
+Output - Text/String
+'''
+def text_converter(file_pth):
+    text = extract_text(
+        open(
+
+            file_pth,
+            'rb'
+            )
+        )
+    return text
